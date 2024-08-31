42f57fe1f43a794776df01ce9b24e6e88bf9c168
VEERPAL SHAH <shah.1764@osu.edu>
2024-08-31 12:54:56 -0400

    Updated next.config.mjs and ensured app directory structure is correct

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack(config) {
-      config.resolve.modules.push(__dirname + '/src');
-      return config;
-    },
-    pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  };
-  
+const nextConfig = {};
+
 export default nextConfig;
-  
