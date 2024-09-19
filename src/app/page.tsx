b92df1f75d853441c96b987bc8aff4752bc7ad9d
Veerpal Shah <89817323+veerpal-shah@users.noreply.github.com>
2024-09-19 16:29:33 -0400

    testing staging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <div className="w-full max-w-3xl p-8 bg-[#c09a7a] border-2 border-[#dbab7f] rounded-lg">
         <h1 className="text-4xl font-bold text-center mb-8">Hollywood Movies and More</h1>
         <p className="text-center text-lg mb-8">
-          What movie am I thinking of? Does it exist? Let’s find out!
+          What movie am I thinking of? Does it exist? Let’s NOT find out!
         </p>
         
         {/* Centered Input Box */}
